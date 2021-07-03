ee7eaea8980d76e4bc0b2d24ecf38688e11f30af
thesantatitan <dev19034@iiitd.ac.in>
2021-07-04 02:29:51 +0530

    Add UserCard

---
+++ src/components/UserCard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import {getEmailFromUid, deleteUserFromChat} from '../services/firebaseDbHelper';
+
+const UserCard = (props) => {
+    return ( 
+        <ListItem divider>
+            <ListItemText primary={getEmailFromUid(props.uid)} style={{overflow:"hidden"}}/>
+            <ListItemIcon>
+                <IconButton
+                    onClick={(event) => {
+                        event.preventDefault();
+                        deleteUserFromChat(props.uid,props.chatID);
+                    }}
+                >
+                    <DeleteIcon/>
+                </IconButton>
+            </ListItemIcon>
+        </ListItem>
+     );
+}
+ 
+export default UserCard;
