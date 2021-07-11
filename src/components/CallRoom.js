84bcd9bf898792ef53d08a5e8ed47a0b79a1b7aa
thesantatitan <dev19034@iiitd.ac.in>
2021-07-11 20:14:00 +0530

    Fix Redirect Bug

--- src/components/CallRoom.js
+++ src/components/CallRoom.js
@@ -2,14 +2,14 @@ import React, { useEffect, useState, useRef } from 'react';
 import '../App.css';
 import Peer from "simple-peer";
 import { db } from '../services/firebase';
-import { useLocation, useHistory } from 'react-router-dom';
+import { useLocation, useHistory, Redirect } from 'react-router-dom';
 import { useAuth } from '../services/AuthContext';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Chats from './Chats';
 import VideoComponent from './VideoComponent';
 import CallControls from './CallControls';
-
+import * as ROUTES from '../constants/routes';
 
 const useStyles = makeStyles((theme) => ({
     videos: {
@@ -26,7 +26,7 @@ const useStyles = makeStyles((theme) => ({
     },
     root: {
         height: '100vh',
-        flexWrap:'nowrap',
+        flexWrap: 'nowrap',
     }
 }))
 
@@ -38,14 +38,13 @@ function CallRoom() {
     const authUser = useAuth();
     const classes = useStyles();
 
-    const [yourID, setYourID] = useState(authUser.user.uid);
+    const [yourID, setYourID] = useState();
     const [users, _setUsers] = useState([]);
     const usersRef = useRef([]);
     const setUsers = (data) => {
         usersRef.current = data;
         _setUsers(data);
     };
-    const [inCallRef, setInCallRef] = useState(db.ref('chats/' + location.state.currentChat + '/inCall'));
 
 
     const [stream, _setStream] = useState();
@@ -74,59 +73,62 @@ function CallRoom() {
     };
 
     useEffect(() => {
+        if (location.state) {
+            let newUserIncallCallback = {};
 
-        let newUserIncallCallback = {};
+            const inCallRef = db.ref('chats/' + location.state.currentChat + '/inCall');
 
+            setYourID(authUser.user.uid);
 
-
-        navigator.mediaDevices.getUserMedia({ video: { height: '500px', width: '500px' }, audio: true }).then(stream => {
-            setStream(stream);
-            if (userVideo.current) {
-                userVideo.current.srcObject = streamRef.current;
-            }
-        });
-
-
-
-
-        inCallRef.once('value', (usersInCallOnFirebase) => {
-            let usersList = usersRef.current.slice();
-            usersInCallOnFirebase.forEach((user) => { usersList.push(user.key) });
-            usersList.push(yourID);
-            setUsers(usersList);
-        }).then(() => {
-            inCallRef.child(yourID).set(true);
-            inCallRef.child(yourID).onDisconnect().remove();
-            newUserIncallCallback = inCallRef.on('child_added', (newUser) => {
-                if (!usersRef.current.includes(newUser.key)) {
-                    let tempUsersList = usersRef.current.slice();
-                    tempUsersList.push(newUser.key);
-                    setUsers(tempUsersList);
-
-                    callPeer(newUser.key);
+            navigator.mediaDevices.getUserMedia({ video: { height: '500px', width: '500px' }, audio: true }).then(stream => {
+                setStream(stream);
+                if (userVideo.current) {
+                    userVideo.current.srcObject = streamRef.current;
                 }
             });
-        });
-
-        const userCallingMeCallback = inCallRef.child(yourID).on('child_added', (callingUser) => {
-            let tempMap = new Map(uidToCallingSignalRef.current);
-            tempMap.set(callingUser.key, callingUser.child('callingData').child('signalData').val());
-            setUidToCallingSignal(tempMap);
-            acceptCall(callingUser.key);
-        });
 
 
-        return () => {
-            inCallRef.off('child_added', newUserIncallCallback);
-            inCallRef.child(yourID).off('child_added', userCallingMeCallback);
-            inCallRef.child(yourID).remove();
-            if (streamRef.current) {
-                streamRef.current.getTracks().forEach((track) => { track.stop() });
-            }
-            uidToPeerRef.current.forEach((peer) => {
-                peer.destroy();
+
+
+            inCallRef.once('value', (usersInCallOnFirebase) => {
+                let usersList = usersRef.current.slice();
+                usersInCallOnFirebase.forEach((user) => { usersList.push(user.key) });
+                usersList.push(yourID);
+                setUsers(usersList);
+            }).then(() => {
+                inCallRef.child(yourID).set(true);
+                inCallRef.child(yourID).onDisconnect().remove();
+                newUserIncallCallback = inCallRef.on('child_added', (newUser) => {
+                    if (!usersRef.current.includes(newUser.key)) {
+                        let tempUsersList = usersRef.current.slice();
+                        tempUsersList.push(newUser.key);
+                        setUsers(tempUsersList);
+
+                        callPeer(newUser.key);
+                    }
+                });
             });
-            setUidToPeer(new Map());
+
+            const userCallingMeCallback = inCallRef.child(yourID).on('child_added', (callingUser) => {
+                let tempMap = new Map(uidToCallingSignalRef.current);
+                tempMap.set(callingUser.key, callingUser.child('callingData').child('signalData').val());
+                setUidToCallingSignal(tempMap);
+                acceptCall(callingUser.key);
+            });
+
+
+            return () => {
+                inCallRef.off('child_added', newUserIncallCallback);
+                inCallRef.child(yourID).off('child_added', userCallingMeCallback);
+                inCallRef.child(yourID).remove();
+                if (streamRef.current) {
+                    streamRef.current.getTracks().forEach((track) => { track.stop() });
+                }
+                uidToPeerRef.current.forEach((peer) => {
+                    peer.destroy();
+                });
+                setUidToPeer(new Map());
+            }
         }
     }, []);
 
@@ -134,6 +136,7 @@ function CallRoom() {
 
 
     function callPeer(id) {
+        const inCallRef = db.ref('chats/' + location.state.currentChat + '/inCall');
         const peer = new Peer({
             initiator: true,
             trickle: false,
@@ -186,6 +189,7 @@ function CallRoom() {
     }
 
     function acceptCall(caller) {
+        const inCallRef = db.ref('chats/' + location.state.currentChat + '/inCall');
         const peer = new Peer({
             initiator: false,
             trickle: false,
@@ -234,12 +238,14 @@ function CallRoom() {
     }
 
 
-
+    if (!location.state) {
+        return <Redirect to={ROUTES.MAINROOM} />
+    }
 
     return (
         <Grid className={classes.root} container direction='row'>
             <Grid item style={{ flexGrow: 7 }}>
-                <Grid container direction='column' style={{height:'100vh'}}>
+                <Grid container direction='column' style={{ height: '100vh' }}>
                     <Grid item className={classes.videos}>
                         <Grid container style={{ flexWrap: 'wrap' }} spacing={2}>
                             <Grid item>
@@ -259,7 +265,7 @@ function CallRoom() {
                         </Grid>
                     </Grid>
                     <Grid item className={classes.controls}>
-                        <CallControls stream={stream}/>
+                        <CallControls stream={stream} />
                     </Grid>
                 </Grid>
             </Grid>
